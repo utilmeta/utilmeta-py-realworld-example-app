f90440af95b214c43af6601add085992c6b0c571
UtilMeta <=>
2022-02-16 19:51:04 +0800

    add config.env

---
+++ conduit/config/env/__init__.py
@@ -0,0 +1,14 @@
+from utilmeta.conf import Env
+
+
+class ServiceEnvironment(Env):
+    PRODUCTION: bool
+    OPS_TOKEN: str
+    JWT_SECRET_key: str
+    DATABASE_USER: str = ''
+    DATABASE_PASSWORD: str = ''
+    PUBLIC_ONLY: bool = True
+    TEST_ENV_SUFFIX: str = ''       # be -test when it's test env
+
+
+env = ServiceEnvironment(__file__)
